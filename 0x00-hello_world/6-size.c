73346255a4021e9f76ec8b2dddb19271078bd929
Lachhab1 <mohamed172lachhab@gmail.com>
2023-03-16 13:30:57 -0700

    size of types in c \n stupid things hhh

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,6 +13,6 @@ int main(void)
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(long int));
 	printf("Size of a long long int: %lu", (unsigned long)sizeof(long long int));
 	printf(" byte(s)\n");
-	printf("Size of a float: %lu byte(s)", (unsigned long)sizeof(float));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(float));
 	return	(0);
 }
