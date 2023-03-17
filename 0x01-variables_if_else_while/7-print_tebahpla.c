53a63cacedc5362d6d23be30e7be7b2ccdd5acc9
Lachhab1 <mohamed172lachhab@gmail.com>
2023-03-17 16:36:25 -0700

    base 16

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Succes)
+ */
+
+int main(void)
+{
+	/* asci of 0 is 48 */
+	int n = 48;
+
+	while (n <= 102)
+	{
+		putchar(n);
+		if (n == 57)
+			n += 39;
+		n++;
+	}
+	return (0);
+}
