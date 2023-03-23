bdc5b01236988c2a453e5ebc9848289294f1ffa6
Lachhab1 <mohamed172lachhab@gmail.com>
2023-03-23 12:21:48 -0700

    0..14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * more_numbers - print 10 times from
+ *			0 to 14
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+	int n;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			n = j;
+			if (j > 9)
+			{
+				_putchar(49);
+				n = j % 10;
+			}
+			_putchar(j + 48);
+		}
+		_putchar('\n');
+	}
+}
