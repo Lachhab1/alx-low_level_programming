f0a915bf83a307dd73efc386bd92cb412b3cbbd5
Lachhab1 <mohamed172lachhab@gmail.com>
2023-03-23 11:22:27 -0700

    multiple operation

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - calcul the multiple of two
+ *		numbers
+ * @a: input 1
+ * @b: input 2
+ *
+ * Return: the result of the op type
+ *		integer
+ */
+
+int mul(int a, int b)
+{
+
+	return (a * b);
+}
