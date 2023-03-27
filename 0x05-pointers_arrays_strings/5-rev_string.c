e5735a17dbae06d5dfa14aa15251d5eb5e492355
Lachhab1 <mohamed172lachhab@gmail.com>
2023-03-27 16:17:07 -0700

    reversed string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - prints a string followed by
+ *              new line reversed
+ * @s: a pointer for string
+ */
+
+void rev_string(char *s)
+{
+	int i, j;
+	char temp;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	for (j = 0; j < i / 2; j++)
+	{
+		temp = s[i];
+		*(s + i) = *(s - (i + 1 + j));
+		*(s - (i + 1 + j)) = temp;
+	}
+	_putchar('\n');
+}
