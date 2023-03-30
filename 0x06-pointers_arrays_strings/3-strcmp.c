e1aaa4e50605eaeb4a8d670b328c33b6404c9d4d
Lachhab1 <mohamed172lachhab@gmail.com>
2023-03-30 05:40:02 -0700

    compare

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcmp - compare two chaines
+ *
+ * @s1: chaine 1
+ * @s2: chaine 2
+ *
+ * Return: 0 if equals 1 if first larger
+ *		and -1 if shorter
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (*s1 == *s2)
+	{
+		if (*s1 == '\0')
+			return 0;
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
+		
