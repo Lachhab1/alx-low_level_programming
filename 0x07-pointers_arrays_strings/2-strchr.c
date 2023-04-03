0512acfc5c397a9caa71c3554f06c4de007c6aa8
Lachhab1 <mohamed172lachhab@gmail.com>
2023-04-03 13:11:12 -0700

    the occ function

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strchr - locat a chaine into string
+ *
+ * @s: our string where we want to look the
+ *		the character c
+ * @c: the charcter that we want to look for
+ *		in the string s
+ *
+ * Return: pointer for the first occu and null
+ *		if not
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (NULL);
+}
