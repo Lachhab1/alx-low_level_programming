e05432d65ee99a69214ec25e79d53c0ee21db5f8
Lachhab1 <mohamed172lachhab@gmail.com>
2023-04-03 13:00:08 -0700

    copy n chaine by src pointer to a dest

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _memcpy - copy n element of
+ *		the src to dest
+ * @dest: pointer to chaines table
+ * @src: pointer to chaine of what we
+ *		copy
+ * @n: number of elements of src want cpy
+ * Return: Pointer to dest contain n
+ *		chaine get by src
+ *		and pasted into dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
