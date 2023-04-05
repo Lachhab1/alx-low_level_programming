813e0e91d8fb9b3510d56f2ca6a8042db3949f0b
Lachhab1 <mohamed172lachhab@gmail.com>
2023-04-05 16:23:56 -0700

    chaine printed recurs

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,11 +8,11 @@
 
 void _puts_recursion(char *s)
 {
-	if (s == NULL || s == '\0')
+	if (s == NULL || *s == '\0')
 	{
 		_putchar('\n');
 		return;
 	}
 	_puthchar(*s);
-	_puts_recursion(++s);
+	_puts_recursion(s + 1);
 }
