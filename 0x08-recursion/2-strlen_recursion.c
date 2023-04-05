2f5e0685c7131b6618bbd373d285dc792782ff9f
Lachhab1 <mohamed172lachhab@gmail.com>
2023-04-05 16:57:13 -0700

    the length rec

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - the length of a string
+ *
+ * @s: chaine to get the length
+ *
+ * Return: the number of chars in String
+ */
+
+int _strlen_recursion(char *s)
+{
+
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(s + 1));
