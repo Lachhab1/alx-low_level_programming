68065c2fa674b86d9e8062cd6058c83819079441
Lachhab1 <mohamed172lachhab@gmail.com>
2023-04-11 14:40:49 -0700

    first malloc const

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,14 +12,18 @@ char *create_array(unsigned int size, char c)
 	char *s;
 
 	if (size == 0)
+	{
 		return (NULL);
+	}
 	s = (char *) malloc(sizeof(char) * size);
 	if (s == NULL)
 	{
 		return (NULL);
 	}
 	for (i = 0; i < size; i++)
+	{
 		s[i] = c;
+	}
 
 	return (s);
 }
