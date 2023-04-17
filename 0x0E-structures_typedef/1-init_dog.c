10f7b0af44151e3506ada515b588389f3fd03a19
Lachhab1 <mohamed172lachhab@gmail.com>
2023-04-17 16:08:21 -0700

    dog hoof hoof

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+
+/**
+ * init_dog - initialise the dog with the entries
+ * @d: the dog
+ * @name: props for the dog
+ * @age: same
+ * @owner: the name of the owner
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if(d)
+	{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+	}
+}
