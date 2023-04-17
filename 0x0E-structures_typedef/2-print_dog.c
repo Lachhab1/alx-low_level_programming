732f3916ccc6b46b419823e5b1e552934c49aa6f
Lachhab1 <mohamed172lachhab@gmail.com>
2023-04-17 16:24:20 -0700

    dog hoof hoof

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,11 +10,11 @@ void print_dog(struct dog *d)
 {
 	if (d)
 	{
-		if (d->name)
+		if (d->name != NULL)
 			printf("Name: %s\n", d->name);
 		else
 			printf("Name: (nil)\n");
 		printf("Age: %.6f\n", d->age);
-		printf("Owner: %s\n", d->owner);
+		printf("Owner: %s", d->owner);
 	}
 }
