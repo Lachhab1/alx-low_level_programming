95f45a679d7faf5d52dfe27c39ebf44d11db81e2
Lachhab1 <mohamed172lachhab@gmail.com>
2023-04-20 17:57:55 -0700

    printf list of numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,15 +12,15 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list ap;
 	unsigned int i;
 
+	va_start(ap, n);
 	if (separator == NULL)
 	{
 		separator = " ";
 	}
-	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(ap, i));
-		if (i != (n - 1))
+		if (i < n - 1)
 		{
 			printf("%s", separator);
 		}
