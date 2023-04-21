660211e0ddc5f0c0e0c6958a59a5f7eb4ce61d6d
Lachhab1 <mohamed172lachhab@gmail.com>
2023-04-20 18:58:48 -0700

    print anthing you want

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -5,45 +5,44 @@
  */
 void print_all(const char * const format, ...)
 {
-	unsigned int i = 0;
+	int i;
 	int flag;
-	va_list listArg;
 	char *str;
+	va_list a_list;
 
-	va_start(listArg, format);
+	va_start(a_list, format);
+	i = 0;
 	while (format != NULL && format[i] != '\0')
 	{
 		switch (format[i])
 		{
 			case 'c':
-				printf("%c", va_arg(listArg, char));
-				flag = 1;
-				break;
-			case 'i':
-				printf("%d", va_arg(listArg, int));
-				flag = 1;
-				break;
-			case 'f':
-				printf("%f", va_arg(listArg, float));
-				flag = 1;
-				break;
-			case 's':
-				str = va_arg(listArg, char*);
-				if (str == NULL)
-				{
-					str = "(nil)";
-				}
-				printf("%s", str);
-				flag = 1;
-				break;
-			default:
+				printf("%c", va_arg(a_list, int));
 				flag = 0;
 				break;
+			case 'i':
+				printf("%i", va_arg(a_list, int));
+				flag = 0;
+				break;
+			case 'f':
+				printf("%f", va_arg(a_list, double));
+				flag = 0;
+				break;
+			case 's':
+				str = va_arg(a_list, char*);
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s", str);
+				flag = 0;
+				break;
+			default:
+				flag = 1;
+				break;
 		}
-		if (format[i + 1] != '\0' && flag)
+		if (format[i + 1] != '\0' && flag == 0)
 			printf(", ");
 		i++;
 	}
 	printf("\n");
-	va_end(listArg);
+	va_end(a_list);
 }
